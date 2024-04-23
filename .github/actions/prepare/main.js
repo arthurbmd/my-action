4fe54f147dfb17e7c994ec96bd01a7d0c906c6f8
Arthur Branco <72501618+arthurbmd@users.noreply.github.com>
2024-04-23 16:37:49 -0300

    chamar core

--- .github/actions/prepare/main.js
+++ .github/actions/prepare/main.js
@@ -13,16 +13,16 @@ function run() {
         
         var parseData = JSON.parse(configFile)
 
-        setOutput("buildRunStaticValidation", parseData.workflow.habilitarValidacaoEstatica)
-        setOutput("buildCompile", parseData.workflow.habilitarConstrucao)
-        setOutput("buildRunUnitTests", parseData.workflow.habilitarTestesUnidade)
-        setOutput("buildRunIntegrationTests", parseData.workflow.habilitarTestesIntegracao)
-        setOutput("buildRunSonar", parseData.workflow.habilitarSonar)
-        setOutput("buildPackage", parseData.workflow.habilitarEmpacotamento)
-        setOutput("buildDocker", parseData.workflow.habilitarEmpacotamentoDocker)
-        setOutput("buildPublish", parseData.workflow.habilitarPublicacao)
-        setOutput("buildDebug", parseData.workflow.habilitarDebug)
-        setOutput("buildAutodeploy", parseData.workflow.autodeploy)
+        core.setOutput("buildRunStaticValidation", parseData.workflow.habilitarValidacaoEstatica)
+        core.setOutput("buildCompile", parseData.workflow.habilitarConstrucao)
+        core.setOutput("buildRunUnitTests", parseData.workflow.habilitarTestesUnidade)
+        core.setOutput("buildRunIntegrationTests", parseData.workflow.habilitarTestesIntegracao)
+        core.setOutput("buildRunSonar", parseData.workflow.habilitarSonar)
+        core.setOutput("buildPackage", parseData.workflow.habilitarEmpacotamento)
+        core.setOutput("buildDocker", parseData.workflow.habilitarEmpacotamentoDocker)
+        core.setOutput("buildPublish", parseData.workflow.habilitarPublicacao)
+        core.setOutput("buildDebug", parseData.workflow.habilitarDebug)
+        core.setOutput("buildAutodeploy", parseData.workflow.autodeploy)
 
     } catch (err) {
         core.notice("falha ao ler arquivo de configuracao")
