0dd54afabc56525f7fa4ecaf295a0c6d7f7a95bd
Arthur Branco <72501618+arthurbmd@users.noreply.github.com>
2024-04-23 13:53:44 -0300

    fix

--- .github/actions/prepare/main.js
+++ .github/actions/prepare/main.js
@@ -4,33 +4,35 @@ const github = require('@actions/github')
 
 const fs = require('fs')
 
-const path = core.getInput('PATH')
+const filePath = core.getInput('PATH')
 
 function run() {
-    core.notice("Current directory:", __dirname)
+    core.notice(filePath)
     try {
-        const configFile = fs.readFile("config.json")
+        const configFile = fs.readFile(filePath)
+        
+        const parseData = JSON.parse(configFile)
+
+        var configs = {
+            "buildRunStaticValidation": parseData.workflow.habilitarValidacaoEstatica,
+            "buildCompile": parseData.workflow.habilitarConstrucao,
+            "buildRunUnitTests": parseData.workflow.habilitarTestesUnidade,
+            "buildRunIntegrationTests": parseData.workflow.habilitarTestesIntegracao,
+            "buildRunSonar": parseData.workflow.habilitarSonar,
+            "buildPackage": parseData.workflow.habilitarEmpacotamento,
+            "buildDocker": parseData.workflow.habilitarEmpacotamentoDocker,
+            "buildPublish": parseData.workflow.habilitarPublicacao,
+            "buildDebug": parseData.workflow.habilitarDebug,
+            "buildAutodeploy": parseData.workflow.autodeploy
+        }
+
+    core.setOutput('configs', configs)
+
     } catch (err) {
         core.notice("falha ao ler arquivo de configuracao")
         core.setFailed(err)
     }
-    const parseData = JSON.parse(configFile)
-
-    var configs = {
-        "buildRunStaticValidation": parseData.workflow.habilitarValidacaoEstatica,
-        "buildCompile": parseData.workflow.habilitarConstrucao,
-        "buildRunUnitTests": parseData.workflow.habilitarTestesUnidade,
-        "buildRunIntegrationTests": parseData.workflow.habilitarTestesIntegracao,
-        "buildRunSonar": parseData.workflow.habilitarSonar,
-        "buildPackage": parseData.workflow.habilitarEmpacotamento,
-        "buildDocker": parseData.workflow.habilitarEmpacotamentoDocker,
-        "buildPublish": parseData.workflow.habilitarPublicacao,
-        "buildDebug": parseData.workflow.habilitarDebug,
-        "buildAutodeploy": parseData.workflow.autodeploy
-    }
-
-    core.setOutput('configs', configs)
-
+    
 }
 
 run()
